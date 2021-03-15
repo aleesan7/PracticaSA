1a955f1b543724db34f4e0de7791bcdbef19c262
aleesan7 <ka_sanchezsamayoa@outlook.com>
2021-03-14 18:34:35 -0600

    Agregando pruebas en ESB con mocha y chai

---
+++ ClientMicroservice/test/test.js
@@ -0,0 +1,110 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const express = require('express');
+const cors = require('cors');
+//const app = require('../index')
+
+const expect = chai.expect
+chai.use(chaiHttp)
+chai.should();
+//app.use(cors());
+
+//app.use(express.urlencoded({ extended: true }));
+//app.use(express.json());
+
+describe("POST /order", () => {
+
+	it("should return status 200", function(done)  {
+        chai.request('http://localhost:7000')
+            .post('/client/order')
+            .send({
+                'clientID': '201114336',
+	            'Restaurant': 'Pizza Hut',
+	            'Menu': 'Pizza de peperoni'
+            })
+            .end(function(err, response)
+            {
+                response.should.have.status(200);
+                response.should.be.json;
+                console.log('Obtained status: '+response.status);
+                done();
+            });
+       
+	});
+});
+
+describe("GET /getorderstatus", () => {
+
+	it("should return status 200", function(done)  {
+        chai.request('http://localhost:7000')
+            .get('/client/getorderstatus?OrderID=1')
+            .end(function(err, response)
+            {
+                response.should.have.status(200);
+                console.log('Obtained status: '+response.status);
+                done();
+            });
+       
+	});
+});
+
+
+/*
+describe("PUT /updateProductName", () => {
+
+	it("should return status 200", function(done)  {
+        chai.request('http://localhost:3000')
+            .put('/updateProductName')
+            .send({
+                'ProductName': 'MrSushi',
+                'idCatalog': 18
+            })
+            .end(function(err, response)
+            {
+                response.should.have.status(200);
+                response.should.be.json;
+                console.log('Obtained status: '+response.status);
+                done();
+            });
+       
+	});
+});
+
+describe("PUT /updateProductImagePath", () => {
+
+	it("should return status 200", function(done)  {
+        chai.request('http://localhost:3000')
+            .put('/updateProductImagePath')
+            .send({
+                'ImagePath': 'https://www.google.com',
+                'idCatalog': 18
+            })
+            .end(function(err, response)
+            {
+                response.should.have.status(200);
+                response.should.be.json;
+                console.log('Obtained status: '+response.status);
+                done();
+            });
+	});
+});
+
+describe("PUT /updateProductDescription", () => {
+
+	it("should return status 200", function(done)  {
+        chai.request('http://localhost:3000')
+            .put('/updateProductDescription')
+            .send({
+                'Description': 'Test Description',
+                'idCatalog': 18
+            })
+            .end(function(err, response)
+            {
+                response.should.have.status(200);
+                response.should.be.json;
+                console.log('Obtained status: '+response.status);
+                done();
+            });
+	});
+});*/
+
